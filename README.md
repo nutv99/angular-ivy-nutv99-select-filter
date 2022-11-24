4f4068b0a742fa2deb7038230ae6b669c8315086
nutv99 <nutv99@gmail.com>
2022-11-24 18:09:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-nutv99-select-filter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-b4ng6k)
---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  showList: boolean = false;
+  heroesA = [
+    { id: 1, name: 'Superman' },
+    { id: 2, name: 'Batman' },
+    { id: 5, name: 'BatGirl' },
+    { id: 3, name: 'Robin' },
+    { id: 4, name: 'Flash' },
+  ];
+
+  heroesB = this.heroesA;
+
+  FilterHeroes(e: any) {
+    this.showList = true;
+    let st = e.target.value;
+    //this.heroesB = this.heroesB.filter(this.isBigEnough(e.target.value))
+    this.heroesB = this.heroesA.filter((entry) => entry.name.includes(st));
+    console.log(this.heroesB);
+  }
+
+  hideList() {
+    this.showList = false;
+  }
+
+  setshowList() {
+    this.showList = true;
+  }
+}
